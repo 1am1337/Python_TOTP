c80f66a2a7c670400ee63e4ab0716428d3105a5c
1am1337 <oskar.nitzschker@icloud.com>
2025-01-17 18:30:25 +0100

    switched gui libary

--- generator.py
+++ generator.py
@@ -1,6 +1,11 @@
 import time
 import pyotp 
-import dearpygui.dearpygui as dpg
+from nicegui import ui
+
+ui.label('Hello NiceGUI!')
+
+ui.run()
+
 import csv
 names = [] 
 keys  = []
@@ -11,22 +16,3 @@ with open('usr_data.csv', newline='') as csvfile:
     	names.append(row["name"]) 
     	keys.append(row["key"])
 
-dpg.create_context()
-
-def button_callback(sender, app_data):
-    print(f"sender is: {sender}")
-    print(f"app_data is: {app_data}")
-
-with dpg.window(label="Tutorial"):
-    dpg.add_button(label="Print to Terminal", callback=button_callback)
-
-dpg.create_viewport(title='Custom Title', width=800, height=600)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
-# below replaces, start_dearpygui()
-while dpg.is_dearpygui_running():
-    dpg.render_dearpygui_frame()
-
-dpg.destroy_context()
