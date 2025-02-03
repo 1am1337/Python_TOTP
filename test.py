cf78252d9f8d4ac0fa15d87325d28ee30f56d1a5
1am1337 <oskar.nitzschker@icloud.com>
2025-02-03 11:11:46 +0100

    prep to merge to main

--- test.py
+++ test.py
@@ -1,12 +1,42 @@
 import tkinter as tk
+from tkinter import * 
+from tkinter.ttk import *
 
-root = tk.Tk()  # Create the main window
 
-def volume_up():
-    print("Volume Increased +1")
 
-# Create the volume up button
-vol_up = tk.Button(root, text="+", command=volume_up)
-vol_up.pack()
+root = Tk()
+
+root.title("TTK Bootstrap! Combobox")
+root.geometry('500x350')
+
+# Create button click function
+def clicker():
+    my_label.config(text=f"You Clicked On {my_combo.get()}!")
+
+# Create Binding function
+def click_bind(e):
+    my_label.config(text=f"You Clicked On {my_combo.get()}!")
+
+# Create Label
+my_label = Label(root, text="Hello World!", font=("Helvetica", 18))
+my_label.pack(pady=30)
+
+# Create Dropdown options
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# Create Combobox
+my_combo = Combobox(root, values=days)
+my_combo.pack(pady=20)
+
+# Set Combo Default
+my_combo.current(0)
+
+# Create a button
+my_button = Button(root, text="Click Me!", command=clicker)
+my_button.pack(pady=20)
+
+# Bind the combobox
+my_combo.bind('<<ComboboxSelected>>', click_bind)
+
 
 root.mainloop()
